3af7a5c03cf6eae81ce6000e37f78e5653ee137c
WON-DER <wonderfilledyt@gmail.com>
2021-09-24 10:35:32 -0700

    cleanup of profile.js

--- src/components/profile.js
+++ src/components/profile.js
@@ -5,22 +5,6 @@ import axios from "axios";
 export default function Profile() {
   //   const [artistRequest, setArtistRequest] = useState("");
 
-  async function getResults(e) {
-    // console.log(
-    //   `https://api.deezer.com/search?q=artist:${encodeURI(artistRequest)}`
-    // );
-    // const result = await axios.get(
-    //   `https://api.deezer.com/search?q=artist:${encodeURI(artistRequest)}`,
-    //   {
-    //     headers: {
-    //       "Access-Control-Allow-Origin": "*",
-    //     },
-    //   }
-    // );
-    // console.log(result);
-    // setResults(result.type);
-  }
-
   return (
     <Fragment>
       <div className="profile-container">
