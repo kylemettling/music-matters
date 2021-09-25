831d929b9e310a8105d196c05f1ac1526ac0fd5b
WON-DER <wonderfilledyt@gmail.com>
2021-09-25 11:11:01 -0700

    added react-beautiful-dnd to project

---
+++ src/components/song.js
@@ -0,0 +1,16 @@
+// import "./search.css";
+import { Fragment, useState } from "react";
+import axios from "axios";
+
+export default function Song() {
+  //   const [artistRequest, setArtistRequest] = useState("");
+
+  return (
+    <Fragment>
+      <div className="profile-container">
+        <div>Song Placehold</div>
+        <div>This will be a song!</div>
+      </div>
+    </Fragment>
+  );
+}
