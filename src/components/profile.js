d2a14a1a4a7d2f0223bb2f49038fecb232940c9e
WON-DER <wonderfilledyt@gmail.com>
2021-09-26 11:14:21 -0700

    added savedSongs component

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,5 +1,6 @@
 // import "./search.css";
 import { Fragment, useState } from "react";
+import SavedSongs from "../components/savedSongs";
 import axios from "axios";
 
 export default function Profile() {
@@ -10,6 +11,7 @@ export default function Profile() {
       <div className="profile-container">
         <div>Profile Placehold</div>
         <div>Saved Songs</div>
+        <SavedSongs />
       </div>
     </Fragment>
   );
