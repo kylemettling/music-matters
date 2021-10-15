b7a3cfb9081f63d4f900e81faf411febd628c867
WON-DER <wonderfilledyt@gmail.com>
2021-10-15 13:05:58 -0700

    restyle of track card

--- src/components/Track.js
+++ src/components/Track.js
@@ -37,37 +37,46 @@ export function Track() {
 	return (
 		<div className='track-main'>
 			{/* <div>{JSON.stringify(track)}</div> */}
-			<div className='track-card-cover'></div>
-			<div
-				className='track-card'
-				style={{ backgroundImage: `url(${track.images.background})` }}
-			>
-				<div className='track-track-details'>
-					<h2>
-						<span className='track-text'>{track.title}</span>
-					</h2>
-					<h3 className='subtitle'>
-						<span className='track-text track'>{track.subtitle}</span>
-					</h3>
-					{/* <span>Album: {track.url}</span> */}
-				</div>
-				<div className='track-album-details'>
-					<div className='track-text'>
-						<img
-							className='album-cover'
-							src={
-								track.images.coverart
-									? track.images.coverart
-									: 'https://is5-ssl.mzstatic.com/image/thumb/Features115/v4/cc/62/0c/cc620ccb-c10d-c538-ce73-06cf185b3303/mzl.ynbraxen.jpg/800x800cc.jpg'
-							}
-							alt={[track.title] + ' cover'}
-						></img>
+			<div className='track-card-cover'>
+				<div
+					className='track-card'
+					style={{ backgroundImage: `url(${track.images.background})` }}
+				>
+					<div className='track-track-details'>
+						<h2>
+							<div className='track-text-con'>
+								<span className='track-text bump-text-track'>
+									{track.title}
+								</span>
+							</div>
+						</h2>
+						<div className='track-text-con'>
+							<h3 className='subtitle'>
+								<span className='track-text track bump-text'>
+									{track.subtitle}
+								</span>
+							</h3>
+						</div>
+						{/* <span>Album: {track.url}</span> */}
 					</div>
-					<h3 className=''>
-						<span className='album-text'>
-							{track.sections[0].metadata[0].text}
-						</span>
-					</h3>
+					<div className='track-album-details'>
+						<div className='track-text'>
+							<img
+								className='album-cover'
+								src={
+									track.images.coverart
+										? track.images.coverart
+										: 'https://is5-ssl.mzstatic.com/image/thumb/Features115/v4/cc/62/0c/cc620ccb-c10d-c538-ce73-06cf185b3303/mzl.ynbraxen.jpg/800x800cc.jpg'
+								}
+								alt={[track.title] + ' cover'}
+							></img>
+						</div>
+						<div className='album-text-con'>
+							<h3>
+								<span className=''>{track.sections[0].metadata[0].text}</span>
+							</h3>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
