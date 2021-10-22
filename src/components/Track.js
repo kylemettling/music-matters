725c7e9b58fe990669124b0f5e1a7cb4e19e31dd
WON-DER <wonderfilledyt@gmail.com>
2021-10-22 13:18:56 -0700

    reworked Track.js API calls

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,140 +1,130 @@
-import axios from "axios";
-import "./track.css";
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router";
-import { shazam, spotify } from "./config/Connection";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import axios from 'axios'
+import './track.css'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router'
+import { shazam, spotify } from './config/Connection'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 // import { SpotifyToken } from './SpotifyToken'
 // // import { useTrack } from './hooks/useTrack'
 // const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 // const clientSecret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
 
 export function Track() {
-  const { id } = useParams();
-  const [track, setTrack] = useState("");
-  const [trackFeatures, setTrackFeatures] = useState({});
-  const [artistDetails, setArtistDetails] = useState({});
-  const [artistImageURL, setArtistImageURL] = useState("");
-  const {
-    location: {
-      state: { token },
-    },
-  } = useHistory();
-  // const [spotifyToken, setSpotifyToken] = useState({})
-  //   const [spotifySearchData, setSpotifySearchData] = useState("no data");
+	const { id } = useParams()
+	const [track, setTrack] = useState('')
+	const [trackFeatures, setTrackFeatures] = useState({})
+	const [artistDetails, setArtistDetails] = useState({})
+	const [artistImageURL, setArtistImageURL] = useState('')
+	const {
+		location: {
+			state: { token },
+		},
+	} = useHistory()
+	// const [spotifyToken, setSpotifyToken] = useState({})
+	//   const [spotifySearchData, setSpotifySearchData] = useState("no data");
 
-  // GET basic track details
-  async function getTrack() {
-    // console.log("TOKEN:", token, id);
-    const options = {
-      method: "GET",
-      url: spotify.urls.getTrack + id,
-      // params: { key: id, locale: "en-US" },
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-      method: "GET",
-    };
-    const search = await axios.request(options);
-    const res = await search.data;
-    setTrack(search.data);
-    setArtistImageURL(search.data.artists[0].href);
-    // try {
-    //   const search = await axios.request(options);
-    //   setTrack(search.data);
-    //   setArtistImageURL(search.data.artists[0].href);
-    // } catch (err) {
-    //   console.error(err);
-    // }
-    getTrackFeatures();
-  }
+	// GET basic track details
+	async function getTrack() {
+		// console.log("TOKEN:", token, id);
+		const options = {
+			method: 'GET',
+			url: spotify.urls.getTrack + id,
+			// params: { key: id, locale: "en-US" },
+			headers: {
+				Authorization: 'Bearer ' + token,
+			},
+		}
+		const search = await axios.request(options)
+		const res = await search.data
+		setTrack(search.data)
+		setArtistImageURL(search.data.artists[0].href)
+		// try {
+		//   const search = await axios.request(options);
+		//   setTrack(search.data);
+		//   setArtistImageURL(search.data.artists[0].href);
+		// } catch (err) {
+		//   console.error(err);
+		// }
+		getTrackFeatures()
+	}
 
-  // GET track audio features
-  async function getTrackFeatures() {
-    // console.log("TOKEN:", token, id);
-    const options = {
-      method: "GET",
-      url: spotify.urls.getTrackFeatures + id,
-      // params: { key: id, locale: "en-US" },
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-      method: "GET",
-    };
-    const search = await axios(options);
-    try {
-      const search = await axios.request(options);
-      // console.log(search.data);
-      // const oldData = {...track}
-      const featureData = await search.data;
-      // const oldData = { ...track };
-      // console.log("huh", track);
-      setTrackFeatures(featureData);
-      // const trackData = { ...(track + search["data"]) };
-      // setTrack({ ...oldData, featureData });
-      // console.log(featureData);
-    } catch (err) {
-      console.error(err);
-    }
+	// GET track audio features
+	async function getTrackFeatures() {
+		// console.log("TOKEN:", token, id);
+		const options = {
+			method: 'GET',
+			url: spotify.urls.getTrackFeatures + id,
+			// params: { key: id, locale: "en-US" },
+			headers: {
+				Authorization: 'Bearer ' + token,
+			},
+		}
+		try {
+			const search = await axios.request(options)
+			const featureData = await search.data
+			setTrackFeatures(featureData)
+		} catch (err) {
+			console.error(err)
+		}
 
-    getArtistDetails();
-  }
+		getArtistDetails()
+	}
 
-  async function getArtistDetails() {
-    // console.log(track.artists);
-    console.log("DETAILS", trackFeatures, artistImageURL, track.astists);
-    const options = {
-      method: "GET",
-      url: artistImageURL,
-      // url: track.artists[0].href,
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    };
+	async function getArtistDetails() {
+		// console.log(track.artists);
+		console.log('DETAILS', trackFeatures, artistImageURL, track.astists)
+		const options = {
+			method: 'GET',
+			url: artistImageURL,
+			// url: track.artists[0].href,
+			headers: {
+				Authorization: 'Bearer ' + token,
+			},
+		}
 
-    // const res = await axios(track.artists[0].href, options).catch((err) => {
-    //   console.log(err);
-    // });
-    // const data = await res.data;
-    // const data = await res.data;
-    // console.log(data);
-    // setArtistDetails(data);
-    // try {
-    //   const search = await axios.request(options);
-    //   console.log(search);
-    //   const data = await search.data;
-    // } catch (err) {
-    //   console.log(err);
-    // }
-    // const artistData = await search.data;
-    // console.log(artistData);
-  }
+		// const res = await axios(track.artists[0].href, options).catch((err) => {
+		//   console.log(err);
+		// });
+		// const data = await res.data;
+		// const data = await res.data;
+		// console.log(data);
+		// setArtistDetails(data);
+		// try {
+		//   const search = await axios.request(options);
+		//   console.log(search);
+		//   const data = await search.data;
+		// } catch (err) {
+		//   console.log(err);
+		// }
+		// const artistData = await search.data;
+		// console.log(artistData);
+	}
 
-  useEffect(() => {
-    // getSpotifyToken();
+	useEffect(() => {
+		// getSpotifyToken();
 
-    getTrack();
-    // getTrackFeatures();
-    // getArtistDetails();
-    // console.log(track);
-  }, []);
+		getTrack()
+		// getTrackFeatures();
+		// getArtistDetails();
+		// console.log(track);
+	}, [])
 
-  if (!track.name) return null;
+	if (!track.name) return null
 
-  return (
-    <div className="track-main">
-      <div className="test">
-        {/* <p>{JSON.stringify(track, undefined, 4)}</p> */}
-      </div>
-      <div className="test">
-        <img
-          className="album-cover"
-          style={{ height: "640px" }}
-          src={track.album.images[0]?.url}
-        ></img>
-        <p>Artist Details: {JSON.stringify(artistDetails)}</p>
-      </div>
-      {/* <div className="track-card-cover">
+	return (
+		<div className='track-main'>
+			<div className='test'>
+				{/* <p>{JSON.stringify(track, undefined, 4)}</p> */}
+			</div>
+			<div className='test'>
+				<img
+					className='album-cover'
+					style={{ height: '640px' }}
+					src={track.album.images[0]?.url}
+				></img>
+				<p>Artist Details: {JSON.stringify(artistDetails)}</p>
+			</div>
+			{/* <div className="track-card-cover">
         <div
           className="track-card"
           style={{ backgroundImage: `url(${track.album.images.url})` }}
@@ -176,6 +166,6 @@ export function Track() {
       <div>
         <h3>SPOTIFY</h3>
       </div> */}
-    </div>
-  );
+		</div>
+	)
 }
