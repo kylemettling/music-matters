f7636a4b77ac60a3aeff0ccc072c1d189178ede9
WON-DER <wonderfilledyt@gmail.com>
2021-10-23 15:21:07 -0700

    partial rework of Track.js requests

--- src/components/Track.js
+++ src/components/Track.js
@@ -4,10 +4,6 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router'
 import { shazam, spotify } from './config/Connection'
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
-// import { SpotifyToken } from './SpotifyToken'
-// // import { useTrack } from './hooks/useTrack'
-// const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-// const clientSecret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
 
 export function Track() {
 	const { id } = useParams()
@@ -20,152 +16,137 @@ export function Track() {
 			state: { token },
 		},
 	} = useHistory()
-	// const [spotifyToken, setSpotifyToken] = useState({})
-	//   const [spotifySearchData, setSpotifySearchData] = useState("no data");
 
-	// GET basic track details
 	async function getTrack() {
-		// console.log("TOKEN:", token, id);
 		const options = {
 			method: 'GET',
 			url: spotify.urls.getTrack + id,
-			// params: { key: id, locale: "en-US" },
 			headers: {
 				Authorization: 'Bearer ' + token,
 			},
 		}
 		const search = await axios.request(options)
-		const res = await search.data
-		setTrack(search.data)
-		setArtistImageURL(search.data.artists[0].href)
-		// try {
-		//   const search = await axios.request(options);
-		//   setTrack(search.data);
-		//   setArtistImageURL(search.data.artists[0].href);
-		// } catch (err) {
-		//   console.error(err);
-		// }
+		const trackData = await search.data
+		// console.log(trackData)
+		setTrack(trackData)
 		getTrackFeatures()
+		// setArtistImageURL(trackData.artists[0].href)
 	}
 
 	// GET track audio features
 	async function getTrackFeatures() {
-		// console.log("TOKEN:", token, id);
 		const options = {
 			method: 'GET',
 			url: spotify.urls.getTrackFeatures + id,
-			// params: { key: id, locale: "en-US" },
 			headers: {
 				Authorization: 'Bearer ' + token,
 			},
 		}
-		try {
-			const search = await axios.request(options)
-			const featureData = await search.data
-			setTrackFeatures(featureData)
-		} catch (err) {
-			console.error(err)
-		}
+		const search = await axios.request(options).catch((err) => console.log(err))
+		const featureData = await search.data
+		setTrackFeatures(featureData)
+		console.log('Artist detail ready:', track.artists)
+		// try {
+		// 	const search = await axios.request(options)
+		// 	const featureData = await search.data
+		// 	setTrackFeatures(featureData)
+		// 	console.log('Artist detail ready:', track.artists)
+		// } catch (err) {
+		// 	console.error(err)
+		// }
 
 		getArtistDetails()
 	}
 
 	async function getArtistDetails() {
 		// console.log(track.artists);
-		console.log('DETAILS', trackFeatures, artistImageURL, track.astists)
+		// console.log('DETAILS', trackFeatures, artistImageURL, track.artists)
+		// console.log('IMAGE URL:', artistImageURL)
 		const options = {
 			method: 'GET',
-			url: artistImageURL,
-			// url: track.artists[0].href,
+			// url: artistImageURL,
+			url: track.artists[0].href,
 			headers: {
 				Authorization: 'Bearer ' + token,
 			},
 		}
-
-		// const res = await axios(track.artists[0].href, options).catch((err) => {
-		//   console.log(err);
-		// });
-		// const data = await res.data;
-		// const data = await res.data;
-		// console.log(data);
-		// setArtistDetails(data);
+		const search = await axios.request(options).catch((err) => console.log(err))
+		// console.log(search)
+		const artistData = await search.data
+		console.log('ARTIST DATA', artistData)
+		setArtistDetails(artistData)
+		// setArtistImageURL
+		// console.log('Artist detail ready:', track)
 		// try {
-		//   const search = await axios.request(options);
-		//   console.log(search);
-		//   const data = await search.data;
+		// 	const search = await axios.request(options)
+		// 	const artistData = await search.data
+		// 	setArtistDetails(artistData)
 		// } catch (err) {
-		//   console.log(err);
+		// 	console.log(err)
 		// }
-		// const artistData = await search.data;
-		// console.log(artistData);
 	}
 
 	useEffect(() => {
-		// getSpotifyToken();
-
+		// if (id) {
+		// 	getTrack()
+		// }
+		// Promise.all([getTrack(), getTrackFeatures(), getArtistDetails()])
 		getTrack()
-		// getTrackFeatures();
-		// getArtistDetails();
-		// console.log(track);
+		// if (track) {
+		// 	getTrackFeatures()
+		// }
 	}, [])
 
-	if (!track.name) return null
+	if (!track && !artistDetails) return null
 
 	return (
 		<div className='track-main'>
-			<div className='test'>
-				{/* <p>{JSON.stringify(track, undefined, 4)}</p> */}
+			<div className='track-card-cover'>
+				<div
+					className='track-card'
+					// style={{
+					// 	backgroundImage: `url(${
+					// 		artistDetails.images[0].url || artistDetails.images.url
+					// 	})`,
+					// }}
+				>
+					<div className='track-track-details'>
+						<div className='track-text-con'>
+							<h2>
+								<span className='track-text bump-text-track'>{track.name}</span>
+							</h2>
+						</div>
+						<div className='track-text-con'>
+							<h3 className='subtitle'>
+								<span className='track-text track bump-text'>
+									{track.artists[0]?.name}
+								</span>
+							</h3>
+						</div>
+					</div>
+					<div className='track-album-details'>
+						<div className='track-text'>
+							<img
+								className='album-cover'
+								src={
+									track.album.images[0]?.url
+										? track.album.images[0]?.url
+										: 'https://is5-ssl.mzstatic.com/image/thumb/Features115/v4/cc/62/0c/cc620ccb-c10d-c538-ce73-06cf185b3303/mzl.ynbraxen.jpg/800x800cc.jpg'
+								}
+								alt={[track.name] + ' cover'}
+							></img>
+						</div>
+						<div className='album-text-con'>
+							{/* <h3>
+								<span className=''>{track.sections[0].metadata[0].text}</span>
+							</h3> */}
+						</div>
+					</div>
+				</div>
 			</div>
-			<div className='test'>
-				<img
-					className='album-cover'
-					style={{ height: '640px' }}
-					src={track.album.images[0]?.url}
-				></img>
-				<p>Artist Details: {JSON.stringify(artistDetails)}</p>
+			<div>
+				<h3>{JSON.stringify(artistDetails)}</h3>
 			</div>
-			{/* <div className="track-card-cover">
-        <div
-          className="track-card"
-          style={{ backgroundImage: `url(${track.album.images.url})` }}
-        >
-          <div className="track-track-details">
-            <h2>
-              <div className="track-text-con">
-                <span className="track-text bump-text-track">{track.name}</span>
-              </div>
-            </h2>
-            <div className="track-text-con">
-              <h3 className="subtitle">
-                <span className="track-text track bump-text">
-                  {track.subtitle}
-                </span>
-              </h3>
-            </div>
-          </div>
-          <div className="track-album-details">
-            <div className="track-text">
-              <img
-                className="album-cover"
-                src={
-                  track.images.coverart
-                    ? track.images.coverart
-                    : "https://is5-ssl.mzstatic.com/image/thumb/Features115/v4/cc/62/0c/cc620ccb-c10d-c538-ce73-06cf185b3303/mzl.ynbraxen.jpg/800x800cc.jpg"
-                }
-                alt={[track.title] + " cover"}
-              ></img>
-            </div>
-            <div className="album-text-con">
-              <h3>
-                <span className="">{track.sections[0].metadata[0].text}</span>
-              </h3>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <h3>SPOTIFY</h3>
-      </div> */}
 		</div>
 	)
 }
