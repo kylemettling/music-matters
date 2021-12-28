bda391c45646f25bca3bbcd4f5a5702f70d59819
kylemettling <58491427+kylemettling@users.noreply.github.com>
2021-12-28 15:52:32 -0800

    *finally* solved artistCoverURL request

--- src/components/TrackDetail.js
+++ src/components/TrackDetail.js
@@ -1,155 +1,123 @@
-import axios from 'axios'
-import './track.css'
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router'
-import { shazam, spotify } from './config/Connection'
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
-import { useTrack } from './hooks'
-import { useAppState } from './../state'
+import axios from "axios";
+import "./track.css";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import { shazam, spotify } from "./config/Connection";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useTrack } from "./hooks";
+import { useAppState } from "./../state";
 
 export function TrackDetail() {
-	const { id } = useParams()
-	const [track, setTrack] = useState('')
-	const [trackFeatures, setTrackFeatures] = useState({})
-	const [artistURL, setArtistURL] = useState('')
-	const [artistCover, setArtistCover] = useState('')
-	const {
-		songTitle,
-		songArtist,
-		// songLength,
-		// songYear,
-		songAlbum,
-		// artistImage,
-		albumCoverURL,
-		artistCoverURL,
-		setTrackDetails,
-	} = useAppState()
-	const {
-		location: {
-			state: { token },
-		},
-	} = useHistory()
+  const { id } = useParams();
+  const [track, setTrack] = useState("");
+  const [trackFeatures, setTrackFeatures] = useState({});
+  const [artistURL, setArtistURL] = useState("");
+  const [artistCover, setArtistCover] = useState("");
+  const {
+    songTitle,
+    songArtist,
+    // songLength,
+    // songYear,
+    songAlbum,
+    // artistImage,
+    albumCoverURL,
+    artistCoverURL,
+    getTrackDetails,
+    getArtistCoverURL,
+  } = useAppState();
+  const {
+    location: {
+      state: { token },
+    },
+  } = useHistory();
 
-	async function getTrack() {
-		const options = {
-			method: 'GET',
-			url: spotify.urls.getTrack + id,
-			headers: {
-				Authorization: 'Bearer ' + token,
-			},
-		}
-		const fetchTrack = async () => {
-			const search = await axios.request(options)
-			const trackData = await search.data
-			setTrack(trackData)
-			setTrackDetails(trackData, token)
-			console.log(trackData)
-		}
-		fetchTrack()
-		if (track) {
-			setArtistURL(track.artists[0].url)
-		}
-		// console.log(artistURL)
-	}
+  async function getTrack() {
+    const options = {
+      method: "GET",
+      url: spotify.urls.getTrack + id,
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    };
+    const fetchTrack = async () => {
+      const search = await axios.request(options);
+      const trackData = await search.data;
+      const url = trackData.artists[0].href;
+      getArtistCoverURL(url, token);
+      getTrackDetails(trackData, token);
+    };
+    fetchTrack();
+  }
 
-	// GET track audio features
-	async function getTrackFeatures() {
-		// console.log(artistURL)
-		const options = {
-			method: 'GET',
-			url: spotify.urls.getTrackFeatures + id,
-			headers: {
-				Authorization: 'Bearer ' + token,
-			},
-		}
-		const fetchTrackFeatures = async () => {
-			const search = await axios
-				.request(options)
-				.catch((err) => console.log(err))
-			const featureData = await search.data
-			setTrackFeatures(featureData)
-		}
-		fetchTrackFeatures()
-	}
+  // GET track audio features
+  async function getTrackFeatures() {
+    const options = {
+      method: "GET",
+      url: spotify.urls.getTrackFeatures + id,
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    };
+    const fetchTrackFeatures = async () => {
+      const search = await axios
+        .request(options)
+        .catch((err) => console.log(err));
+      const featureData = await search.data;
+      setTrackFeatures(featureData);
+    };
+    fetchTrackFeatures();
+  }
 
-	async function getArtistDetails() {
-		// console.log(track)
-		// const options = {
-		// 	method: 'GET',
-		// 	url: track.artists[0].url,
-		// 	headers: {
-		// 		Authorization: 'Bearer ' + token,
-		// 	},
-		// }
-		// console.log(options)
-		// const fetchArtistDetails = async () => {
-		// 	const search = await axios
-		// 		.request(options)
-		// 		.catch((err) => console.log(err))
-		// 	const artistData = search.data
-		// 	const artistCoverURL = artistData.images[0]?.url
-		// 	console.log(artistCoverURL)
-		// 	setArtistCover(artistCoverURL)
-		// }
-		// fetchArtistDetails()
-	}
+  useEffect(() => {
+    getTrack();
+    getTrackFeatures();
+  }, []);
 
-	useEffect(() => {
-		if (!track) {
-			getTrack()
-			getTrackFeatures()
-			// setTrackDetails(track)
-		}
-		if (track) {
-			getArtistDetails()
-		}
-	}, [])
+  if (!track && !artistURL && !trackFeatures) return null;
+  // if (!track ) return null
 
-	if (!track && !artistURL && !trackFeatures) return null
-	// if (!track ) return null
-
-	return (
-		<div>
-			<div className='track-main'>
-				<div className='track-card-cover'>
-					<div
-						className='track-card'
-						style={{
-							backgroundImage: `url(${artistCoverURL})`,
-						}}
-					>
-						<div className='track-track-details'>
-							<div className='track-text-con'>
-								<h2>
-									<span className='track-text bump-text-track'>
-										{songTitle}
-									</span>
-								</h2>
-							</div>
-							<div className='track-text-con'>
-								<h3 className='subtitle'>
-									<span className='track-text track bump-text'>
-										{songArtist}
-									</span>
-								</h3>
-							</div>
-						</div>
-						<div className='track-album-details'>
-							<div className='track-text'>
-								<img
-									className='album-cover'
-									src={albumCoverURL}
-									alt={[songAlbum] + ' cover'}
-								></img>
-							</div>
-							<div className='album-text-con'>{songAlbum}</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className='audio-features'>
-				{JSON.stringify(trackFeatures, null, 4)}
-			</div>
-		</div>
-	)
+  return (
+    <div>
+      <div className="track-main">
+        <div className="track-card-cover">
+          <div
+            className="track-card"
+            style={{
+              backgroundImage: `url(${artistCoverURL})`,
+            }}
+          >
+            <div className="track-track-details">
+              <div className="track-text-con">
+                <h2>
+                  <span className="track-text bump-text-track">
+                    {songTitle}
+                  </span>
+                </h2>
+              </div>
+              <div className="track-text-con">
+                <h3 className="subtitle">
+                  <span className="track-text track bump-text">
+                    {songArtist}
+                  </span>
+                </h3>
+              </div>
+            </div>
+            <div className="track-album-details">
+              <div className="track-text">
+                <img
+                  className="album-cover"
+                  src={albumCoverURL}
+                  alt={[songAlbum] + " cover"}
+                ></img>
+              </div>
+              <div className="album-text-con">{songAlbum}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="audio-features">
+        {JSON.stringify(trackFeatures, null, 4)}
+      </div>
+    </div>
+  );
 }
--- src/components/hooks/useTrack.js
+++ src/components/hooks/useTrack.js
@@ -1,64 +1,51 @@
-import { useState } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export const useTrack = () => {
-	const [track, setTrack] = useState()
-	const [songTitle, setSongTitle] = useState('')
-	const [songArtist, setSongArtist] = useState('')
-	const [songAlbum, setSongAlbum] = useState('')
-	const [albumCoverURL, setAlbumCoverURL] = useState('')
-	const [artistCoverURL, setArtistCoverURL] = useState('')
-	// const [track, setTrack] = useState({})
+  const [songTitle, setSongTitle] = useState("");
+  const [songArtist, setSongArtist] = useState("");
+  const [songAlbum, setSongAlbum] = useState("");
+  const [albumCoverURL, setAlbumCoverURL] = useState("");
+  const [artistCoverURL, setArtistCoverURL] = useState("");
 
-	const setTrackDetails = (data, token) => {
-		// console.log(data.album.images)
-		setTrack(data)
-		const artistName = data.artists[0].name
-		const albumCover = data.album.images[0].url
-		// const artistCover = data.artists[0].images[0].url
-		// console.log(albumCover)
-		setSongTitle(data.name)
-		setSongArtist(artistName)
-		setSongAlbum(data.album.name)
-		// console.log(songTitle, songAlbum, songArtist)
-		// const songArtist =
-		// const albumURL = albumCover
-		// console.log(albumCover[0].url)
-		setAlbumCoverURL(albumCover)
-
-		const getArtistCoverURL = (data) => {
-			const options = {
-				method: 'GET',
-				url: data.artists[0].url,
-				headers: {
-					Authorization: 'Bearer ' + token,
-				},
-			}
-			console.log('OPTIONS: ', options)
-			const fetchArtistDetails = async () => {
-				const search = await axios
-					.request(options)
-					.catch((err) => console.log(err))
-				const artistData = await search.data
-				const artistCover = artistData.images[0]?.url
-				console.log(artistCover)
-				setArtistCoverURL(artistCover)
-			}
-			fetchArtistDetails()
-		}
-		getArtistCoverURL(data)
-		// setArtistCoverURL(artistCover)
-		// return { songTitle, songArtist, songAlbum, albumCoverURL }
-	}
-
-	// useEffect(() => {}, [albumCoverURL])
-
-	return {
-		songTitle,
-		songArtist,
-		songAlbum,
-		albumCoverURL,
-		artistCoverURL,
-		setTrackDetails,
-	}
-}
+  const getTrackDetails = (data, token) => {
+    const setTrackDetails = (data) => {
+      const artistName = data.artists[0].name;
+      const albumCover = data.album.images[0].url;
+      setSongTitle(data.name);
+      setSongArtist(artistName);
+      setSongAlbum(data.album.name);
+      setAlbumCoverURL(albumCover);
+    };
+    setTrackDetails(data);
+  };
+  const getArtistCoverURL = (data, token) => {
+    const options = {
+      method: "GET",
+      url: data,
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    };
+    console.log("OPTIONS: ", options);
+    const fetchArtistDetails = async () => {
+      const search = await axios
+        .request(options)
+        .catch((err) => console.log(err));
+      const artistData = await search.data;
+      const artistCover = artistData.images[0].url;
+      console.log(artistCover);
+      setArtistCoverURL(artistCover);
+    };
+    fetchArtistDetails();
+  };
+  return {
+    songTitle,
+    songArtist,
+    songAlbum,
+    albumCoverURL,
+    artistCoverURL,
+    getTrackDetails,
+    getArtistCoverURL,
+  };
+};
