3ce80d18b09d7192168e7ffce068f1707b8e0685
kylemettling <58491427+kylemettling@users.noreply.github.com>
2021-12-29 11:01:38 -0800

    added updated PageWrapper.js

--- src/state/PageWrapper.js
+++ src/state/PageWrapper.js
@@ -1,28 +1,37 @@
-import React, { createContext, useContext } from 'react'
-import { useSpotifyToken, useTrack } from '../components/hooks'
+import React, { createContext, useContext } from "react";
+import { useSpotifyToken, useTrack } from "../components/hooks";
 
-export const AppContext = createContext({})
+export const AppContext = createContext({});
 
 export const PageWrapper = ({ children }) => {
-	const { token, refreshToken, getStoredToken } = useSpotifyToken()
-	const { songTitle, songArtist, songAlbum, albumCoverURL, setTrackDetails } =
-		useTrack()
-	return (
-		<AppContext.Provider
-			value={{
-				token,
-				refreshToken,
-				getStoredToken,
-				songTitle,
-				songArtist,
-				songAlbum,
-				albumCoverURL,
-				setTrackDetails,
-			}}
-		>
-			{children}
-		</AppContext.Provider>
-	)
-}
+  const { token, refreshToken, getStoredToken } = useSpotifyToken();
+  const {
+    songTitle,
+    songArtist,
+    songAlbum,
+    albumCoverURL,
+    artistCoverURL,
+    getTrackDetails,
+    getArtistCoverURL,
+  } = useTrack();
+  return (
+    <AppContext.Provider
+      value={{
+        token,
+        refreshToken,
+        getStoredToken,
+        songTitle,
+        songArtist,
+        songAlbum,
+        albumCoverURL,
+        artistCoverURL,
+        getTrackDetails,
+        getArtistCoverURL,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 
-export const useAppState = () => useContext(AppContext)
+export const useAppState = () => useContext(AppContext);
