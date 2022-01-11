bc495959df06567edd359022506b68ae718b2ce7
kylemettling <58491427+kylemettling@users.noreply.github.com>
2022-01-10 16:15:04 -0800

    EOD push

--- src/components/hooks/useScaleChords.js
+++ src/components/hooks/useScaleChords.js
@@ -1,35 +1,39 @@
-import { useState, useEffect } from 'react'
-import { useAppState } from '../../state'
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
-import { spotify } from '../config/Connection'
-import axios from 'axios'
-// import scaleNotes from './../../state/scaleNotes'
+import { useState, useEffect } from "react";
+import { useAppState } from "../../state";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { spotify } from "../config/Connection";
+import axios from "axios";
+import scaleNotes from "./../../state/scaleNotes";
+import scaleChordStructure from "../../state/scaleChordStructure";
 
 export const useScaleChords = () => {
-	function getScaleChords(root, scales, chordStructure, mode) {
-		const scaleNotes = scales[root].notes
-		const scaleChordStructure = chordStructure[mode]
-		// const scaleChords = scaleNotes.map((note, idx) => [
-		// 	{
-		// 		id: idx + 1,
-		// 		root: note,
-		// 		type: scaleChordStructure[idx],
-		// 		position: idx + 1,
-		// 	},
-		// ])
-		const scaleChords = scaleNotes.map((note, idx) => {
-			return {
-				id: idx + 1,
-				root: note,
-				type: scaleChordStructure[idx],
-				position: idx + 1,
-			}
-		})
-		console.log('Scale Chords created', scaleChords)
-		return scaleChords
-	}
+  function getScaleChords(root, mode) {
+    //   function getScaleChords(root, scales, chordStructure, mode) {
+    const notes = scaleNotes[root]?.notes;
+    console.log(notes);
+    const structure = scaleChordStructure[mode];
+    console.log(scaleChordStructure);
+    // const scaleChords = scaleNotes.map((note, idx) => [
+    // 	{
+    // 		id: idx + 1,
+    // 		root: note,
+    // 		type: scaleChordStructure[idx],
+    // 		position: idx + 1,
+    // 	},
+    // ])
+    const scaleChords = notes.map((note, idx) => {
+      return {
+        id: idx + 1,
+        root: note,
+        type: structure[idx],
+        position: idx + 1,
+      };
+    });
+    console.log("Scale Chords created", scaleChords);
+    return scaleChords;
+  }
 
-	return {
-		getScaleChords,
-	}
-}
+  return {
+    getScaleChords,
+  };
+};
--- src/components/hooks/useTrack.js
+++ src/components/hooks/useTrack.js
@@ -1,121 +1,131 @@
-import { useState, useEffect } from 'react'
-import { useAppState } from '../../state'
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
-import { spotify } from '../config/Connection'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import { useAppState } from "../../state";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { spotify } from "../config/Connection";
+import axios from "axios";
 
 export const useTrack = () => {
-	const [isActiveTrack, setIsActiveTrack] = useState(false)
-	const [songTitle, setSongTitle] = useState('')
-	const [songArtist, setSongArtist] = useState('')
-	const [songAlbum, setSongAlbum] = useState('')
-	const [songKey, setSongKey] = useState('')
-	const [songKeyCenterQuality, setKeyCenterQuality] = useState('')
+  const [isActiveTrack, setIsActiveTrack] = useState(false);
+  const [songTitle, setSongTitle] = useState("");
+  const [songArtist, setSongArtist] = useState("");
+  const [songAlbum, setSongAlbum] = useState("");
+  const [songKey, setSongKey] = useState("");
+  const [songKeyCenterQuality, setKeyCenterQuality] = useState("");
 
-	const [spotifySongId, setSpotifySongId] = useState('')
-	const [albumCoverURL, setAlbumCoverURL] = useState('')
-	const [artistURL, setArtistURL] = useState('')
-	const [isStoredTrack, setIsStoredTrack] = useState(false)
-	const { token, refreshToken, getStoredToken } = useAppState()
+  const [spotifySongId, setSpotifySongId] = useState("");
+  const [albumCoverURL, setAlbumCoverURL] = useState("");
+  const [artistURL, setArtistURL] = useState("");
+  const [isStoredTrack, setIsStoredTrack] = useState(false);
+  const { token, refreshToken, getStoredToken } = useAppState();
 
-	const [artistCover, setArtistCover] = useState({
-		url: '',
-		h: '',
-		w: '',
-	})
-	const history = useHistory()
-	function setTrackFeatures(data) {
-		setSongKey(data.key)
-		setKeyCenterQuality(data.mode)
-	}
+  const [artistCover, setArtistCover] = useState({
+    url: "",
+    h: "",
+    w: "",
+  });
+  const history = useHistory();
+  // function setTrackFeatures(data) {
+  // 	setSongKey(data.key)
+  // 	setKeyCenterQuality(data.mode)
+  // }
 
-	async function getTrackFeatures(id, token) {
-		if (id !== '' && token !== '') {
-			const options = {
-				method: 'GET',
-				url: spotify.urls.getTrackFeatures + id,
-				headers: {
-					Authorization: 'Bearer ' + token,
-				},
-			}
-			console.log('GET TRACK FEATURES ID:', id)
-			const fetchTrackFeatures = async () => {
-				const search = await axios
-					.request(options)
-					.catch((err) => console.log(err))
-				const featureData = await search.data
-				setTrackFeatures(featureData)
-			}
-			fetchTrackFeatures()
-		}
-	}
-	// const getTrackDetails = (data) => {
-	const setTrack = (data, token) => {
-		const artistName = data.artists[0].name
-		const albumCover = data.album.images[0].url
-		const url = data.artists[0].href
-		setSpotifySongId(data.id)
-		setSongTitle(data.name)
-		setSongArtist(artistName)
-		setSongAlbum(data.album.name)
-		setAlbumCoverURL(albumCover)
-		setArtistURL(url)
-		getArtistCoverURL(url, token)
-		getTrackFeatures(data.id, token)
-		setIsActiveTrack(true)
+  async function getTrackFeatures(id, token) {
+    if (id !== "" && token !== "") {
+      const options = {
+        method: "GET",
+        url: spotify.urls.getTrackFeatures + id,
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      };
+      console.log("GET TRACK FEATURES ID:", id);
+      const fetchTrackFeatures = async () => {
+        const search = await axios
+          .request(options)
+          .catch((err) => console.log(err));
 
-		// localStorage.setItem('trackId')
-	}
-	const getArtistCoverURL = (data, token) => {
-		if (data !== '' && token !== '') {
-			const options = {
-				method: 'GET',
-				url: data,
-				headers: {
-					Authorization: 'Bearer ' + token,
-				},
-			}
-			const fetchArtistDetails = async () => {
-				const search = await axios
-					.request(options)
-					.catch((err) => console.log(err))
-				const artistData = await search.data
-				const artistCover = artistData.images[0]
-				setArtistCover({
-					url: artistCover.url,
-					h: artistCover.height,
-					w: artistCover.width,
-				})
-			}
-			fetchArtistDetails()
-		}
-	}
+        if (!search) {
+          refreshToken();
+          getTrackFeatures(id, token);
+        }
 
-	const clearTrackData = () => {
-		setSpotifySongId('')
-		setSongTitle('')
-		setSongArtist('')
-		setSongAlbum('')
-		setAlbumCoverURL('')
-		getArtistCoverURL('', '')
-		getTrackFeatures('', '')
-		setSongKey('')
-		setKeyCenterQuality('')
-		setIsActiveTrack(false)
-	}
-	return {
-		songTitle,
-		songArtist,
-		songAlbum,
-		songKey,
-		songKeyCenterQuality,
-		spotifySongId,
-		albumCoverURL,
-		artistURL,
-		artistCover,
-		setTrack,
-		isStoredTrack,
-		isActiveTrack,
-		clearTrackData,
-	}
-}
+        const featureData = await search.data;
+        // setTrackFeatures(featureData)
+
+        // console.log(featureData.key, featureData.mode);
+        setSongKey(featureData.key);
+        setKeyCenterQuality(featureData.mode);
+      };
+      fetchTrackFeatures();
+    }
+  }
+  // const getTrackDetails = (data) => {
+  const setTrack = (data, token) => {
+    const artistName = data.artists[0].name;
+    const albumCover = data.album.images[0].url;
+    const url = data.artists[0].href;
+    setSpotifySongId(data.id);
+    setSongTitle(data.name);
+    setSongArtist(artistName);
+    setSongAlbum(data.album.name);
+    setAlbumCoverURL(albumCover);
+    setArtistURL(url);
+    getArtistCoverURL(url, token);
+    getTrackFeatures(data.id, token);
+    setIsActiveTrack(true);
+
+    // localStorage.setItem('trackId')
+  };
+  const getArtistCoverURL = (data, token) => {
+    if (data !== "" && token !== "") {
+      const options = {
+        method: "GET",
+        url: data,
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      };
+      const fetchArtistDetails = async () => {
+        const search = await axios
+          .request(options)
+          .catch((err) => console.log(err));
+        const artistData = await search.data;
+        const artistCover = artistData.images[0];
+        setArtistCover({
+          url: artistCover.url,
+          h: artistCover.height,
+          w: artistCover.width,
+        });
+      };
+      fetchArtistDetails();
+    }
+  };
+
+  const clearTrackData = () => {
+    setSpotifySongId("");
+    setSongTitle("");
+    setSongArtist("");
+    setSongAlbum("");
+    setAlbumCoverURL("");
+    getArtistCoverURL("", "");
+    getTrackFeatures("", "");
+    setSongKey("");
+    setKeyCenterQuality("");
+    setIsActiveTrack(false);
+  };
+  return {
+    songTitle,
+    songArtist,
+    songAlbum,
+    songKey,
+    songKeyCenterQuality,
+    spotifySongId,
+    albumCoverURL,
+    artistURL,
+    artistCover,
+    setTrack,
+    isStoredTrack,
+    isActiveTrack,
+    clearTrackData,
+  };
+};
