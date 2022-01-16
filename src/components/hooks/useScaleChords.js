2d8e66cfa1d99e9881fc58944b9f3d6c20d4acf8
WON-DER <wonderfilledyt@gmail.com>
2022-01-15 19:09:13 -0800

    step towards more accurate music thoery results

--- src/components/hooks/useScaleChords.js
+++ src/components/hooks/useScaleChords.js
@@ -6,19 +6,33 @@ import axios from 'axios'
 import scaleNotes from './../../state/scaleNotes'
 import scaleChordStructure from '../../state/scaleChordStructure'
 import { ChordImage } from './../ChordImage'
+import notes from './../../state/notes'
+const rawNotes = notes
 
 export const useScaleChords = () => {
 	function getScaleChords(root, mode) {
 		console.log('root in getScaleChords', root)
 		const notes = scaleNotes[root]?.notes
-		const structure = scaleChordStructure[`${mode.toLowerCase()}`]
-		console.log('Structure!', structure, notes)
+		const structure = scaleChordStructure[`${mode.toLowerCase()}`].notes
+
+		// index of current scale
+		const indexOfScale = scaleChordStructure[`${mode.toLowerCase()}`].degree
+
+		console.log(
+			'🚀 ~ file: useScaleChords.js ~ line 18 ~ getScaleChords ~ indexOfScale',
+			indexOfScale
+		)
 
 		function handleQuality(chordType) {
 			const newType =
 				chordType === 'min' ? 'm' : chordType === 'maj' ? '' : chordType
 			return newType
 		}
+
+		// map over available notes -> point current note to index of desired scale
+		// index of scale = scaleChordStructure[`${mode.toLowerCase()}`]
+		// index of degree(I) relation to destined mode (II - VII)
+		//
 
 		const scaleChords = notes.map((note, idx) => {
 			console.log('structure:', note, structure[idx], idx)
@@ -31,7 +45,16 @@ export const useScaleChords = () => {
 				// image: <ChordImage chordName={root + handleQuality(structure[idx])} />,
 			}
 		})
-		console.log('Scale Chords created', scaleChords)
+		console.log(
+			'Index of Scale:',
+			indexOfScale,
+			'Notes presented:',
+			rawNotes,
+			'root',
+			root,
+			'mode',
+			mode
+		)
 		return scaleChords
 	}
 
--- src/state/scaleChordStructure.js
+++ src/state/scaleChordStructure.js
@@ -1,11 +1,32 @@
 const scaleChordStructure = {
-	ionian: ['maj', 'min', 'min', 'maj', 'maj', 'min', 'dim'],
-	dorian: ['min', 'min', 'maj', 'maj', 'min', 'dim', 'maj'],
-	phrygian: ['min', 'maj', 'maj', 'min', 'dim', 'maj', 'min'],
-	lydian: ['maj', 'maj', 'min', 'dim', 'maj', 'min', 'min'],
-	mixolydian: ['maj', 'min', 'dim', 'maj', 'min', 'min', 'maj'],
-	aeolian: ['min', 'dim', 'maj', 'min', 'min', 'maj', 'maj'],
-	locrian: ['dim', 'maj', 'min', 'min', 'maj', 'maj', 'min'],
+	ionian: {
+		notes: ['maj', 'min', 'min', 'maj', 'maj', 'min', 'dim'],
+		degree: 1,
+	},
+	dorian: {
+		notes: ['min', 'min', 'maj', 'maj', 'min', 'dim', 'maj'],
+		degree: 2,
+	},
+	phrygian: {
+		notes: ['min', 'maj', 'maj', 'min', 'dim', 'maj', 'min'],
+		degree: 3,
+	},
+	lydian: {
+		notes: ['maj', 'maj', 'min', 'dim', 'maj', 'min', 'min'],
+		degree: 4,
+	},
+	mixolydian: {
+		notes: ['maj', 'min', 'dim', 'maj', 'min', 'min', 'maj'],
+		degree: 5,
+	},
+	aeolian: {
+		notes: ['min', 'dim', 'maj', 'min', 'min', 'maj', 'maj'],
+		degree: 6,
+	},
+	locrian: {
+		notes: ['dim', 'maj', 'min', 'min', 'maj', 'maj', 'min'],
+		degree: 7,
+	},
 }
 
 // const scaleChordStructure = [
