010b3dc90a7cade5fd8096f244a8e61676fba659
kylemettling <58491427+kylemettling@users.noreply.github.com>
2022-03-03 16:21:57 -0800

    changes to rawNotes.js

--- src/components/hooks/useChordbook.js
+++ src/components/hooks/useChordbook.js
@@ -1,60 +1,89 @@
-import { useState } from 'react'
-import { useAppState } from '../../state'
+import { useState } from "react";
+import { useAppState } from "../../state";
 
 export const useChordbook = () => {
-	const [token, setToken] = useState('')
-	const { getScaleChords } = useAppState()
-	const [chordbooks, setChordbooks] = useState([])
-	const [chordCount, setChordCount] = useState(0)
-	// chord list
-	// const updateChordList = (chordList, chords) => {
-	// 	const prev = chordList.slice()
-	// 	setChordbooks((prevState) => [...prevState, ...chordList])
-	// }
-	const createStartingBook = (key, quality) => {
-		console.log('KEY/QUALITY', key, quality)
-		const copy = chordbooks.slice()
-		const starterBook = {
-			id: 0,
-			name: 'suggested scale',
-			root: key,
-			mode: quality,
-			type: 'starter',
-			bookId: 0,
-			chords: getScaleChords(key, quality),
-		}
-		const blankChord = {
-			id: 7,
-			root: 'blank',
-			type: '',
-			position: 1,
-			degree: 'above to start',
-		}
-		const introBook = {
-			id: 1,
-			name: 'intro',
-			root: key,
-			mode: quality,
-			type: 'intro',
-			bookId: 1,
-			chords: [blankChord],
-		}
-		const newList = [...copy, starterBook, introBook]
-		// console.log(
-		// 	'chord count?',
-		// 	newList.reduce((acc, curr) => acc + curr.chords.length, chordCount)
-		// )
-		const newChordCount = newList.reduce(
-			(acc, curr) => acc + curr.chords.length,
-			chordCount
-		)
-		console.log('done with book', newList, newChordCount)
-		setChordbooks(newList)
-		setChordCount(newChordCount)
-	}
-	const sanitizeIds = () => {
-		// console.log(chordCount)
-	}
+  const [token, setToken] = useState("");
+  const { getScaleChords } = useAppState();
+  const [chordbooks, setChordbooks] = useState([]);
+  const [chordCount, setChordCount] = useState(0);
+  const [bookCount, setBookCount] = useState(0);
+  // chord list
+  // const updateChordList = (chordList, chords) => {
+  // 	const prev = chordList.slice()
+  // 	setChordbooks((prevState) => [...prevState, ...chordList])
+  // }
+  const createStartingBook = (key, quality) => {
+    console.log("KEY/QUALITY", key, quality);
+    const copy = chordbooks.slice();
+    const starterBook = {
+      id: 0,
+      name: "suggested scale",
+      root: key,
+      mode: quality,
+      type: "starter",
+      bookId: 0,
+      chords: getScaleChords(key, quality),
+    };
+    const blankChord = {
+      id: 7,
+      root: "",
+      type: "blank",
+      position: 1,
+      degree: "above to start",
+    };
 
-	return { chordbooks, createStartingBook, setChordbooks, sanitizeIds }
-}
+    const introBook = {
+      id: 1,
+      name: "intro",
+      root: key,
+      mode: quality,
+      type: "intro",
+      bookId: 1,
+      chords: [blankChord],
+    };
+
+    const newList = [...copy, starterBook, introBook];
+    const newChordCount = newList.reduce(
+      (acc, curr) => acc + curr.chords.length,
+      chordCount
+    );
+    console.log("done with book", newList, newChordCount);
+    setChordbooks(newList);
+    setChordCount(newChordCount);
+  };
+  const createIntroBook = () => {
+    const blankChord = {
+      id: 7,
+      root: "",
+      type: "blank",
+      position: 1,
+      degree: "above to start",
+    };
+
+    const introBook = {
+      id: 1,
+      name: "intro",
+      root: "",
+      mode: "",
+      type: "intro",
+      bookId: 1,
+      chords: [blankChord],
+    };
+  };
+  const updateChordbooks = () => {
+    // setChordbooks
+    // setChordCount
+    // setBookCount
+  };
+  const sanitizeIds = () => {
+    // console.log(chordCount)
+  };
+
+  return {
+    chordbooks,
+    createStartingBook,
+    createIntroBook,
+    setChordbooks,
+    sanitizeIds,
+  };
+};
