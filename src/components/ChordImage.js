f22cecaa4014206cb5b07b30d98803698c3435e8
kylemettling <58491427+kylemettling@users.noreply.github.com>
2022-03-09 16:22:03 -0800

    restyling Chord.js

--- src/components/ChordImage.js
+++ src/components/ChordImage.js
@@ -1,57 +1,45 @@
-import React, { useEffect, useState, useRef } from 'react'
-import { GiGuitarHead } from 'react-icons/gi'
-import { useOutsideClick } from './hooks/'
-import './chordImage.css'
+import React, { useEffect, useState, useRef } from "react";
+import "./chordImage.css";
 
 export function ChordImage({ chordName }) {
-	const [isZoomed, setIsZoomed] = useState(false)
-	const [chord, setChordName] = useState(chordName)
-	const imageRef = useRef(null)
-	// const {}
+  const [isZoomed, setIsZoomed] = useState(false);
+  const [chord, setChordName] = useState(chordName);
+  const imageRef = useRef(null);
 
-	function handleImageClick(e) {
-		// const
-		setIsZoomed(!isZoomed)
-		// console.log(isImageToggled)
-	}
+  function handleImageClick(e) {
+    setIsZoomed(!isZoomed);
+  }
 
-	function handleImgName(name) {
-		const newName = name.includes('#') ? name.replace('#', 'sharp') : name
-		// console.log(newName);
-		return newName
-	}
+  function handleImgName(name) {
+    const newName = name.includes("#") ? name.replace("#", "sharp") : name;
+    return newName;
+  }
 
-	useEffect(() => {
-		// if (!toggle) {
-		// 	console.log(toggle)
-		// }
-		// if (toggle) {
-		// 	console.log(toggle)
-		// }
-		setChordName(chordName)
-	}, [chordName, chord])
+  useEffect(() => {
+    setChordName(chordName);
+  }, [chordName]);
 
-	return (
-		<div className='chord-image-con'>
-			{chord && (
-				<div className={`chord-image`}>
-					<img
-						ref={imageRef}
-						onClick={handleImageClick}
-						src={`/img/${handleImgName(chord)}.svg`}
-						alt={`${chord}-img`}
-					/>
-					{isZoomed && (
-						<div className={`chord-image${isZoomed ? '-zoom' : ''}`}>
-							<img
-								onClick={handleImageClick}
-								src={`/img/${handleImgName(chord)}.svg`}
-								alt={`${chord}-img-zoom`}
-							/>
-						</div>
-					)}
-				</div>
-			)}
-		</div>
-	)
+  return (
+    <div className="chord-image-con">
+      {chord && (
+        <div className={`chord-image`}>
+          <img
+            ref={imageRef}
+            onClick={handleImageClick}
+            src={`/img/${handleImgName(chord)}.svg`}
+            alt={`${chord}-img`}
+          />
+          {isZoomed && (
+            <div className={`chord-image${isZoomed ? "-zoom" : ""}`}>
+              <img
+                onClick={handleImageClick}
+                src={`/img/${handleImgName(chord)}.svg`}
+                alt={`${chord}-img-zoom`}
+              />
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
--- src/components/hooks/useChordbook.js
+++ src/components/hooks/useChordbook.js
@@ -1,89 +1,121 @@
-import { inRange } from 'lodash'
-import { useState } from 'react'
-import { useAppState } from '../../state'
+import { inRange } from "lodash";
+import { useState } from "react";
+import { useAppState } from "../../state";
 
 export const useChordbook = () => {
-	const { getScaleChords } = useAppState()
-	const [chordbooks, setChordbooks] = useState([])
-	const [chordCount, setChordCount] = useState(0)
-	const [bookCount, setBookCount] = useState(0)
-	const createStartingBook = (key, quality) => {
-		const copy = chordbooks.slice()
-		const starterBook = {
-			id: 0,
-			name: 'suggested scale',
-			root: key,
-			mode: quality,
-			type: 'starter',
-			isErasable: false,
-			bookId: 0,
-			chords: getScaleChords(key, quality),
-		}
-		const blankChord = {
-			id: starterBook.chords.length + 1,
-			root: '',
-			type: 'blank',
-			position: 1,
-			// isErasable: false,
-			degree: 'above to start',
-		}
-		// setChordCount(chordCount + 1)
-		const introBook = {
-			id: 1,
-			name: 'intro',
-			root: key,
-			mode: quality,
-			type: 'intro',
-			bookId: 1,
-			isErasable: true,
-			chords: [blankChord],
-		}
+  const { getScaleChords } = useAppState();
+  const [chordbooks, setChordbooks] = useState([]);
+  const [chordCount, setChordCount] = useState(0);
+  const [bookCount, setBookCount] = useState(0);
+  const createStartingBook = (key, quality) => {
+    const copy = chordbooks.slice();
+    const starterBook = {
+      id: 0,
+      name: "suggested scale",
+      root: key,
+      mode: quality,
+      type: "starter",
+      isErasable: false,
+      bookId: 0,
+      chords: getScaleChords(key, quality),
+    };
+    const blankChord = {
+      id: starterBook.chords.length + 1,
+      root: "drag chord",
+      type: "blank",
+      position: 1,
+      // isErasable: false,
+      degree: "to start",
+    };
+    // setChordCount(chordCount + 1)
+    const introBook = {
+      id: 1,
+      name: "intro",
+      root: key,
+      mode: quality,
+      type: "intro",
+      bookId: 1,
+      isErasable: true,
+      chords: [blankChord],
+    };
 
-		const newList = [...copy, starterBook, introBook]
-		const newChordCount = newList.reduce(
-			(acc, curr) => acc + curr.chords.length,
-			chordCount
-		)
-		setChordbooks(newList)
-		setChordCount(newChordCount)
-	}
-	const updateStarterChordbook = (newChords, bookId) => {
-		// const copy = chordbooks.slice();
-		const copy = [...chordbooks]
-		const update = copy.map((book) => {
-			if (book.bookId === bookId) {
-				book.chords = newChords
-				return book
-			} else {
-				return book
-			}
-		})
-		// console.log("update", update);
-		setChordbooks(update)
-		// setChordCount
-		// setBookCount
-	}
-	const sanitizeIds = () => {
-		// console.log(chordbooks, chordCount);
-		const copy = chordbooks.slice()
-		let count = 0
-		for (const book of copy) {
-			for (let i = 0; i < book.chords.length; i++) {
-				book.chords[i].id = count
-				book.chords[i].position = i + 1
-				count++
-			}
-		}
-		setChordbooks(copy)
-		setChordCount(count)
-	}
+    const newList = [...copy, starterBook, introBook];
+    const newChordCount = newList.reduce(
+      (acc, curr) => acc + curr.chords.length,
+      chordCount
+    );
+    setChordbooks(newList);
+    setChordCount(newChordCount);
+  };
+  const updateStarterChordbook = (newChords, bookId) => {
+    // const copy = chordbooks.slice();
+    const copy = [...chordbooks];
+    const update = copy.map((book) => {
+      if (book.bookId === bookId) {
+        book.chords = newChords;
+        return book;
+      } else {
+        return book;
+      }
+    });
+    // console.log("update", update);
+    setChordbooks(update);
+    // setChordCount
+    // setBookCount
+  };
+  const sanitizeIds = () => {
+    const copy = chordbooks.slice();
+    let count = 0;
+    for (const book of copy) {
+      for (let i = 0; i < book.chords.length; i++) {
+        // console.log(book.chords.length, book.chords[i], i, count);
+        book.chords[i].id = count;
+        book.chords[i].position = i + 1;
+        count++;
+        // console.log(book.chords.length, book.chords[i], i, count);
+      }
+    }
+    setChordbooks(copy);
+    setChordCount(count);
+  };
 
-	return {
-		chordbooks,
-		createStartingBook,
-		// createIntroBook,
-		setChordbooks,
-		updateStarterChordbook,
-		sanitizeIds,
-	}
-}
+  const createBook = () => {
+    const copy = chordbooks.slice();
+    const blankChord = {
+      id: 1,
+      root: "",
+      type: "blank",
+      position: 1,
+      // isErasable: false,
+      degree: "",
+    };
+    const newBook = {
+      id: copy.length + 1,
+      // insert snazzy song part name function here
+      name: "new book",
+      root: "",
+      mode: "",
+      type: "intro",
+      bookId: copy.length + 1,
+      isErasable: true,
+      chords: [blankChord],
+    };
+    const newList = [...copy, newBook];
+    const count = newList.reduce(
+      (acc, curr) => acc + curr.chords.length,
+      chordCount
+    );
+    setChordCount(count);
+    setChordbooks(newList);
+  };
+
+  return {
+    chordbooks,
+    createStartingBook,
+    // createIntroBook,
+    createBook,
+    setChordbooks,
+    updateStarterChordbook,
+    sanitizeIds,
+  };
+};
