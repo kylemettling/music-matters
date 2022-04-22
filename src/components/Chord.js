2a738e0cef4c544d1fe5e1caf167bcab4412f1d5
WON-DER <wonderfilledyt@gmail.com>
2022-04-22 10:12:31 -0700

    fixed chord image zoom click event

--- src/components/Chord.js
+++ src/components/Chord.js
@@ -1,212 +1,186 @@
-import { useEffect } from "react";
-import { Draggable } from "react-beautiful-dnd";
-import { ChordImage } from "./ChordImage";
-import { useState, useRef } from "react";
-import rawNotes from "./../state/rawNotes";
-import "./chord.css";
-import { FaTrashAlt } from "react-icons/fa";
-import { MdContentCopy } from "react-icons/md";
+import { useEffect } from 'react'
+import { Draggable } from 'react-beautiful-dnd'
+import { ChordImage } from './ChordImage'
+import { useState, useRef } from 'react'
+import rawNotes from './../state/rawNotes'
+import './chord.css'
+import { FaTrashAlt } from 'react-icons/fa'
+import { MdContentCopy } from 'react-icons/md'
 
 function Chord({
-  id,
-  position,
-  root,
-  type,
-  degree,
-  _droppableId,
-  updateChord,
+	id,
+	position,
+	root,
+	type,
+	degree,
+	_droppableId,
+	updateChord,
 }) {
-  const [isEditing, setToggleIsEditing] = useState(false);
-  const [chordRoot, setChordRoot] = useState(root);
-  const [chordType, setChordType] = useState(type);
-  const [keyOptionState, setKeyOptionState] = useState(root);
-  const [typeOptionState, setTypeOptionState] = useState(type);
-  const [chordHover, setChordHover] = useState(false);
-  const editRef = useRef();
-  function handleQuality(type) {
-    const newType = type === "min" ? "m" : type === "maj" ? "" : type;
-    return newType;
-  }
-  function handleName(type, root) {
-    const newType = handleQuality(type);
-    return `${root}${newType === "blank" ? "" : newType}`;
-  }
-  function handleEditToggle(e) {
-    if (!isEditing) {
-      setToggleIsEditing(!isEditing);
-    }
-  }
-  function handleChordChange(e) {
-    updateChord(_droppableId, id, "root", e.target.value);
-    setKeyOptionState(e.target.value);
-    setChordRoot(e.target.value);
-    setToggleIsEditing(!isEditing);
-  }
-  function handleTypeChange(e) {
-    updateChord(_droppableId, id, "type", e.target.value);
-    setTypeOptionState(e.target.value);
-    setChordType(e.target.value);
-    setToggleIsEditing(!isEditing);
-  }
+	const [isEditing, setToggleIsEditing] = useState(false)
+	const [isZoomed, setIsZoomed] = useState(false)
+	const [chordRoot, setChordRoot] = useState(root)
+	const [chordType, setChordType] = useState(type)
+	const [keyOptionState, setKeyOptionState] = useState(root)
+	const [typeOptionState, setTypeOptionState] = useState(type)
+	const [chordHover, setChordHover] = useState(false)
+	const editRef = useRef()
+	const zoomRef = useRef()
+	function handleQuality(type) {
+		const newType = type === 'min' ? 'm' : type === 'maj' ? '' : type
+		return newType
+	}
+	function handleName(type, root) {
+		const newType = handleQuality(type)
+		return `${root}${newType === 'blank' ? '' : newType}`
+	}
+	function handleEditToggle(e) {
+		if (!isEditing) {
+			setToggleIsEditing(!isEditing)
+		}
+	}
+	function handleChordChange(e) {
+		updateChord(_droppableId, id, 'root', e.target.value)
+		setKeyOptionState(e.target.value)
+		setChordRoot(e.target.value)
+		setToggleIsEditing(!isEditing)
+	}
+	function handleTypeChange(e) {
+		updateChord(_droppableId, id, 'type', e.target.value)
+		setTypeOptionState(e.target.value)
+		setChordType(e.target.value)
+		setToggleIsEditing(!isEditing)
+	}
 
-  //   function handleChordHover(e) {
-  //     console.log(e.target.classList);
-  //     if (chordHover) {
-  // 		setChordHover(!chordHover);
-  //       e.target.classList.remove(".hover");
-  //     }
-  //   }
+	//   function handleChordHover(e) {
+	//     console.log(e.target.classList);
+	//     if (chordHover) {
+	// 		setChordHover(!chordHover);
+	//       e.target.classList.remove(".hover");
+	//     }
+	//   }
 
-  useEffect(() => {
-    setChordRoot(root);
-    setChordType(type);
-    const checkIfClickedoutside = (e) => {
-      if (editRef.current && !editRef.current.contains(e.target)) {
-        setToggleIsEditing(false);
-      }
-    };
-    document.addEventListener("mousedown", checkIfClickedoutside);
+	useEffect(() => {
+		setChordRoot(root)
+		setChordType(type)
+		const checkIfEditClickedOutside = (e) => {
+			if (editRef.current && !editRef.current.contains(e.target)) {
+				setToggleIsEditing(false)
+			}
+		}
+		document.addEventListener('mousedown', checkIfEditClickedOutside)
 
-    return () => {
-      document.removeEventListener("mousedown", checkIfClickedoutside);
-    };
-  }, [id, position, editRef, root, type]);
-  return (
-    <Draggable draggableId={id.toString()} index={position} key={id}>
-      {(provided) => (
-        <div
-          className={`chord-detail droppableId-${_droppableId} flex card`}
-          //   onMouseEnter={(e) => handleChordHover(e)}
-          //   onMouseLeave={(e) => handleChordHover(e)}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          ref={provided.innerRef}
-        >
-          <div>
-            <div className={`chord-name-edit`} ref={editRef}>
-              <div
-                className="chord-header"
-                onClick={(e) => handleEditToggle(e)}
-              >
-                {(isEditing && (
-                  <div className="key-mode-select-con">
-                    <select
-                      className="chordRootSelect"
-                      name="KeySelector"
-                      id="key_selector"
-                      value={keyOptionState}
-                      onChange={(e) => handleChordChange(e)}
-                    >
-                      {rawNotes.map((note) => (
-                        <option
-                          className="chordRootSelectOption"
-                          value={
-                            // note.length > 1 ? `${note[0]}/${note[1]}` : note
-                            note.length > 1 ? note[0] : note
-                          }
-                        >
-                          {
-                            // note.length > 1 ? `${note[0]}/${note[1]}` : note
-                            note.length > 1 ? note[0] : note
-                          }
-                        </option>
-                      ))}
-                      {/* <option value='C'>C</option>
-											<option value='C#'>C#</option>
-											<option value='Db'>Db</option>
-											<option value='D'>D</option>
-											<option value='D#'>D#</option>
-											<option value='Eb'>Eb</option>
-											<option value='E'>E</option>
-											<option value='F'>F</option>
-											<option value='F#'>F#</option>
-											<option value='Gb'>Gb</option>
-											<option value='G'>G</option>
-											<option value='G#'>G#</option>
-											<option value='Ab'>Ab</option>
-											<option value='A'>A</option>
-											<option value='A#'>A#</option>
-											<option value='Bb'>Bb</option>
-											<option value='B'>B</option> */}
-                    </select>
-                    <select
-                      className="chordRootSelect"
-                      name="KeySelector"
-                      id="key_selector"
-                      value={typeOptionState}
-                      onChange={(e) => handleTypeChange(e)}
-                    >
-                      <option className="chordRootSelectOption" value="maj">
-                        maj
-                      </option>
-                      <option className="chordRootSelectOption" value="maj7">
-                        maj7
-                      </option>
-                      <option className="chordRootSelectOption" value="min">
-                        m
-                      </option>
-                      <option className="chordRootSelectOption" value="m7">
-                        m7
-                      </option>
-                      <option className="chordRootSelectOption" value="7">
-                        7
-                      </option>
-                      <option className="chordRootSelectOption" value="dim">
-                        dim
-                      </option>
-                    </select>
-                  </div>
-                )) || (
-                  <span className="chord-name">
-                    {/* {chordRoot + handleQuality(chordType)} */}
-                    {handleName(chordType, chordRoot)}
-                  </span>
-                )}
-              </div>
-              <ChordImage
-                chordName={
-                  chordType !== "blank"
-                    ? chordRoot + handleQuality(chordType)
-                    : "blank"
-
-                  //   handleName(chordType, chordRoot)
-                }
-              />{" "}
-              <div className="chord-footer flex">
-                <div>
-                  {type !== "blank" ? (
-                    <FaTrashAlt
-                      className={`chord-control ${chordHover ? "active" : ""}`}
-                      style={{ color: "var(--red)" }}
-                    />
-                  ) : null}
-                </div>
-                <div>
-                  {degree && (
-                    <span
-                      className={`chord-degree ${
-                        chordType === "blank" ? "blank" : ""
-                      }`}
-                    >
-                      {degree}
-                    </span>
-                  )}
-                </div>
-                <div>
-                  {type !== "blank" ? (
-                    <MdContentCopy
-                      className={`chord-control ${chordHover ? "active" : ""}`}
-                      //  style={{ color: "var(--background)" }}
-                    />
-                  ) : null}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </Draggable>
-  );
+		return () => {
+			document.removeEventListener('mousedown', checkIfEditClickedOutside)
+		}
+	}, [id, position, editRef, zoomRef, root, type])
+	return (
+		<Draggable draggableId={id.toString()} index={position} key={id}>
+			{(provided) => (
+				<div
+					className={`chord-detail droppableId-${_droppableId} flex card`}
+					{...provided.draggableProps}
+					{...provided.dragHandleProps}
+					ref={provided.innerRef}
+				>
+					<div ref={zoomRef}>
+						<div className={`chord-name-edit`} ref={editRef}>
+							<div
+								className='chord-header'
+								onClick={(e) => handleEditToggle(e)}
+							>
+								{(isEditing && (
+									<div className='key-mode-select-con'>
+										<select
+											className='chordRootSelect'
+											name='KeySelector'
+											id='key_selector'
+											value={keyOptionState}
+											onChange={(e) => handleChordChange(e)}
+										>
+											{rawNotes.map((note, i) => (
+												<option
+													key={i}
+													className='chordRootSelectOption'
+													value={note.length > 1 ? note[0] : note}
+												>
+													{note.length > 1 ? note[0] : note}
+												</option>
+											))}
+										</select>
+										<select
+											className='chordRootSelect'
+											name='KeySelector'
+											id='key_selector'
+											value={typeOptionState}
+											onChange={(e) => handleTypeChange(e)}
+										>
+											<option className='chordRootSelectOption' value='maj'>
+												maj
+											</option>
+											<option className='chordRootSelectOption' value='maj7'>
+												maj7
+											</option>
+											<option className='chordRootSelectOption' value='min'>
+												m
+											</option>
+											<option className='chordRootSelectOption' value='m7'>
+												m7
+											</option>
+											<option className='chordRootSelectOption' value='7'>
+												7
+											</option>
+											<option className='chordRootSelectOption' value='dim'>
+												dim
+											</option>
+										</select>
+									</div>
+								)) || (
+									<span className='chord-name'>
+										{handleName(chordType, chordRoot)}
+									</span>
+								)}
+							</div>
+							<ChordImage
+								chordName={
+									chordType !== 'blank'
+										? chordRoot + handleQuality(chordType)
+										: 'blank'
+								}
+							/>{' '}
+							<div className='chord-footer flex'>
+								<div>
+									{type !== 'blank' ? (
+										<FaTrashAlt
+											className={`chord-control ${chordHover ? 'active' : ''}`}
+											style={{ color: 'var(--red)' }}
+										/>
+									) : null}
+								</div>
+								<div>
+									{degree && (
+										<span
+											className={`chord-degree ${
+												chordType === 'blank' ? 'blank' : ''
+											}`}
+										>
+											{degree}
+										</span>
+									)}
+								</div>
+								<div>
+									{type !== 'blank' ? (
+										<MdContentCopy
+											className={`chord-control ${chordHover ? 'active' : ''}`}
+										/>
+									) : null}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+		</Draggable>
+	)
 }
 
-export default Chord;
+export default Chord
--- src/components/ChordImage.js
+++ src/components/ChordImage.js
@@ -1,45 +1,56 @@
-import React, { useEffect, useState, useRef } from "react";
-import "./chordImage.css";
+import React, { useEffect, useState, useRef } from 'react'
+import './chordImage.css'
 
 export function ChordImage({ chordName }) {
-  const [isZoomed, setIsZoomed] = useState(false);
-  const [chord, setChordName] = useState(chordName);
-  const imageRef = useRef(null);
+	const [isZoomed, setIsZoomed] = useState(false)
+	const [chord, setChordName] = useState(chordName)
+	const imageRef = useRef(null)
+	const zoomRef = useRef(null)
 
-  function handleImageClick(e) {
-    setIsZoomed(!isZoomed);
-  }
+	function handleImageClick(e) {
+		setIsZoomed(!isZoomed)
+	}
 
-  function handleImgName(name) {
-    const newName = name.includes("#") ? name.replace("#", "sharp") : name;
-    return newName;
-  }
+	function handleImgName(name) {
+		const newName = name.includes('#') ? name.replace('#', 'sharp') : name
+		return newName
+	}
 
-  useEffect(() => {
-    setChordName(chordName);
-  }, [chordName]);
+	useEffect(() => {
+		setChordName(chordName)
 
-  return (
-    <div className="chord-image-con">
-      {chord && (
-        <div className={`chord-image`}>
-          <img
-            ref={imageRef}
-            onClick={handleImageClick}
-            src={`/img/${handleImgName(chord)}.svg`}
-            alt={`${chord}-img`}
-          />
-          {isZoomed && (
-            <div className={`chord-image${isZoomed ? "-zoom" : ""}`}>
-              <img
-                onClick={handleImageClick}
-                src={`/img/${handleImgName(chord)}.svg`}
-                alt={`${chord}-img-zoom`}
-              />
-            </div>
-          )}
-        </div>
-      )}
-    </div>
-  );
+		const checkIfZoomClickedOutside = (e) => {
+			if (zoomRef.current && !zoomRef.current.contains(e.target)) {
+				setIsZoomed(false)
+			}
+		}
+		document.addEventListener('mousedown', checkIfZoomClickedOutside)
+	}, [chordName, zoomRef])
+
+	return (
+		<div className='chord-image-con'>
+			{chord && (
+				<div className={`chord-image`}>
+					<img
+						ref={imageRef}
+						onClick={handleImageClick}
+						src={`/img/${handleImgName(chord)}.svg`}
+						alt={`${chord}-img`}
+					/>
+					{isZoomed && (
+						<div
+							className={`chord-image${isZoomed ? '-zoom' : ''}`}
+							ref={zoomRef}
+						>
+							<img
+								onClick={handleImageClick}
+								src={`/img/${handleImgName(chord)}.svg`}
+								alt={`${chord}-img-zoom`}
+							/>
+						</div>
+					)}
+				</div>
+			)}
+		</div>
+	)
 }
